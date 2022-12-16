4b0c83cbaad9d8e78681e91a1870022df4243ebc
unknown <asa006266@gmial.com>
2022-12-16 12:14:22 +0500

    first shop

---
+++ frontend/build/precache-manifest.44d45a4c270ca8777ab6c7e238075b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37899fd8f2da48b475f28be6c49b1728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f69d2f85ea6929c1ad",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f69d2f85ea6929c1ad",
+    "url": "/static/js/main.68482b83.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
